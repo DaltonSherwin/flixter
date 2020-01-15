044f754261ca0948a35d72c8b08b510b0ad19242
DaltonSherwin <daltonsherwin@gmail.com>
2020-01-15 01:30:35 +0000

    Flixter lesson 24

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 # config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV["flixter-dalton-bucket"]
-  config.aws_acl    = "public-read"
-
+ config.storage = :aws
+ config.aws_bucket = ENV["AWS_BUCKET"]
+ config.aws_acl = "public-read" 
+                          # required
   config.aws_credentials = {
-      access_key_id:     ENV["AKIATY6KRIZ53PXJMSOD"],
-      secret_access_key: ENV["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"],
-      region:            ENV["us-east-1"]
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:            ENV["AWS_REGION"] # required
   }
+            # required
 end
