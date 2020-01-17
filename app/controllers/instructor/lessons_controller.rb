077a0320a036a4035f56ab06f7673b1bf7adf008
DaltonSherwin <daltonsherwin@gmail.com>
2020-01-17 00:54:02 +0000

    Flixter lesson 36

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,9 +1,6 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
-
-  
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
@@ -17,12 +14,18 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
